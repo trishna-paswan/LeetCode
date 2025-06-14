40ff4aed3ecbe299541c6ea84ae38a7dbe3bb798
Trishna Paswan <S24CSEU0083@bennett.edu.in>
2025-06-14 12:34:23 +0530

    Create Prob125.cpp

---
+++ Prob125.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string cl = "";
+
+        for(char c : s){
+            if (isalnum(c)){
+                cl += tolower(c);
+            }
+        }
+        int n = cl.length();
+        for(int i=0; i < n/2; i++){
+            if (cl[i] != cl[n-1-i]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
